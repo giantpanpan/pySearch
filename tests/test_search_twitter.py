c08f8989bb6bae0116fcdf91ab09a38eeea29407
wangpan <doria.panwang@gmail.com>
2018-12-07 11:04:54 -0500

    fix pySearch test

---
+++ tests/test_search_twitter.py
@@ -0,0 +1,9 @@
+import pytest
+from ..search import Search
+
+def test_builtLink_twitter():
+   tempSearch = Search()
+   tempSearch.setEngine("twitter")
+   tempSearch.setDomain("com")
+   tempSearch.setQuery(["test","query"])
+   assert tempSearch.buildLink() == "http://www.twitter.com/search?q=test query"
